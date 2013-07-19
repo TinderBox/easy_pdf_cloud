d29efa6daf86ea5e24ff179f1d131540812d82af
Joe Heth <joeheth@gmail.com>
2013-07-19 13:04:36 -0400

    Updating to be compatible with pdf-cloud.com v1 release.

--- lib/pdf_cloud.rb
+++ lib/pdf_cloud.rb
@@ -3,30 +3,49 @@ require 'oauth2'
 
 module PdfCloud
 
-  HOST = "https://api.pdf-cloud.com"
-  VERSION = "v0"
-  WORKFLOW_URL = "#{HOST}/#{VERSION}/workflows"
-  SITE = 'https://www.pdf-cloud.com'
-
   class Client
+
+    # The client id (aka "Consumer Key") to use for OAuth2 authentication
+    attr_accessor :client_id
+    # The client secret (aka "Consumer Secret" to use for OAuth2 authentication)
+    attr_accessor :client_secret
+    # The OAuth access token in use by the client
+    attr_accessor :access_token
+    # The OAuth refresh token in use by the client
+    attr_accessor :refresh_token
+    # The host to use for OAuth2 authentication. Defaults to www.pdf-cloud.com
+    attr_accessor :host
+    # The host to use for API requests. Defaults to api.pdf-cloud.com
+    attr_accessor :api_host
+    # The API version the client is using. Defaults to v1
+    attr_accessor :version
+    # The base URL to the workflow API
+    attr_reader :workflow_url
+    # The base URL to the Base API https://{api_host}/{version}
+    attr_reader :api_url
 
     def initialize(options)
 
       @options = options
-      client_id = options['client_id']
-      client_secret = options['client_secret']
-      access_token = options['access_token']
-      refresh_token = options['refresh_token']
+      @host = options['host'] || 'https://www.pdf-cloud.com'
+      @api_host = options['api_host'] || "https://api.pdf-cloud.com"
+      @version = options['version'] || "v1"
+      @api_url = "#{@api_host}/#{@version}"
+      @workflow_url = "#{@api_url}/workflows"
+      @client_id = options['client_id']
+      @client_secret = options['client_secret']
+      @access_token = options['access_token']
+      @refresh_token = options['refresh_token']
 
       client_options = {
-        :site => SITE,
+        :site => @host,
         :authorize_url    => '/oauth2/authorize',
         :token_url        => '/oauth2/token'
       }
-      @client = OAuth2::Client.new(client_id, client_secret, client_options)
+      @client = OAuth2::Client.new(@client_id, @client_secret, client_options)
       #@client.auth_code.authorize_url(:redirect_uri => callback_url, :scope => "epc.api", :state => "EasyPDFCloud")
 
-      @access_token = OAuth2::AccessToken.from_hash(@client, {:access_token => access_token, :refresh_token => refresh_token})
+      @access_token = OAuth2::AccessToken.from_hash(@client, {:access_token => @access_token, :refresh_token => @refresh_token})
     end
 
     def verify_access_token
@@ -62,118 +81,103 @@ module PdfCloud
 
     def workflows
       check_access_token
-      response = @access_token.get(WORKFLOW_URL)
+      response = @access_token.get(@workflow_url)
       hash = response.parsed
       hash["workflows"]
     end
 
     def workflow_details(id)
-      response = @access_token.get("#{WORKFLOW_URL}/#{id}")
+      response = @access_token.get("#{@workflow_url}/#{id}")
       workflow_id = response.parsed["workflowID"].to_i
-      Workflow.new(@access_token, workflow_id)
+      Workflow.new(self, workflow_id)
     end
 
     def workflow(id)
-      Workflow.new(@access_token, id)
+      Workflow.new(self, id)
     end
 
   end
 
   class Workflow
-    def initialize(access_token, workflow_id, options = {})
-      @access_token = access_token
+    def initialize(client, workflow_id, options = {})
+      @client = client
+      @access_token = client.access_token
       @workflow_id = workflow_id
+      @workflow_url = "#{client.workflow_url}/#{@workflow_id}"
+      @jobs_url = "#{client.api_url}/jobs"
       @event_id = nil
       @debug = options[:debug]
     end
 
     def convert_data(filename, data, source_extension, dest_extension)
-      put_file(filename, data)
-      start_and_wait
+      job_id = create_job_from_file(filename, data)
+      wait_for_completion(job_id)
       output_file = filename.sub(".#{source_extension}", ".#{dest_extension}")
-      response = retrieve_file(output_file)
-      delete_output_file(output_file)
+      response = retrieve_file(job_id, output_file)
+      # The job stays around after execution. We can either delete or keep a history some where.
+      # delete_job(job_id)
+      # Delete the output file so it doesn't take up storage space.
+      delete_output_file(job_id, output_file)
       response.body
     end
 
-    def convert(filename, source_extension, dest_extension)
-      upload(filename)
-      start_and_wait
-      download(filename.sub(".#{source_extension}", ".#{dest_extension}"))
-    end
-
-    # Upload Input File
-    def upload(filepath)
-      raise "Invalid file given for upload #{filepath}" if !File.file?(filepath)
-
-      file_data = File.open(filepath, 'rb') {|f| f.read}
-      filename = File.basename(filepath)
-      put_file(filename, file_data)
-    end
-
-    def put_file(filename, file_data)
-      file_url = "#{WORKFLOW_URL}/#{@workflow_id}/files/input/#{filename}?type=file"
-      response = @access_token.put(file_url, {:body => file_data, :headers => {"Content-Type" => "application/pdf"}})
-      return response.is_a?(Hash)
+    def create_job_from_file(filename, file_data)
+      create_job_url = "#{@workflow_url}/jobs?file=#{filename}"
+      response = @access_token.put(create_job_url, {:body => file_data, :headers => {"Content-Type" => "application/pdf"}})
+      return response.parsed["jobID"]
     end
 
     # Download Output File
-    def download(filename, destination_path = nil)
-      response = retrieve_file(filename)
+    def download(job_id, filename, destination_path = nil)
+      response = retrieve_file(job_id, filename)
       filepath = (destination_path ? File.join(destination_path, filename) : filename)
 
       File.open(filepath, "wb") {|f| f.write(response.body)}
-      delete_output_file(filename)
+      delete_output_file(job_id, filename)
       true
     end
 
-    def retrieve_file(filename)
-      file_url = "#{WORKFLOW_URL}/#{@workflow_id}/files/output/#{filename}"
+    def retrieve_file(job_id, filename)
+      file_url = "#{@jobs_url}/#{job_id}/output/#{filename}"
       response = @access_token.get(file_url)
     end
 
-    def delete_input_file(filename)
-      delete(filename, 'input')
-    end
-
-    def delete_output_file(filename)
-      delete(filename, 'output')
-    end
-
-    # Delete File from location (input/output)
-    def delete(filename, location)
-      file_url = "#{WORKFLOW_URL}/#{@workflow_id}/files/output/#{filename}"
+    # Delete File from output folder.
+    def delete_output_file(job_id, filename)
+      file_url = "#{@jobs_url}/#{job_id}/output/#{filename}"
       response = @access_token.delete(file_url)
       return response.parsed.is_a?(Hash)
     end
 
-    def start_and_wait
-      start_job()
+    # There is no response from this command.
+    def start_job(id)
+      response = @access_token.post("#{@jobs_url}/#{id}")
+      response.status
+    end
+
+    def delete_job(id)
+      response = @access_token.delete("#{@jobs_url}/#{id}")
+      response.status
+    end
+
+    def wait_for_completion(job_id)
       count = 1
-      while (wait_for_completion() == false)
+      while (job_event(job_id) == false)
         if count == 3
-          stop_job()
+          delete_job(job_id)
           raise "Failed to convert after 90 seconds."
         end
         count += 1
       end
     end
 
-    def start_job
-      response = @access_token.post("#{WORKFLOW_URL}/#{@workflow_id}/job")
-      hash = response.parsed
-      @event_id = hash["eventID"]
-      return hash.has_key?("eventID")
+    def job_status(job_id)
+      response = @access_token.get("#{@jobs_url}/#{job_id}")
+      response.parsed
     end
 
-    def stop_job
-      response = @access_token.delete("#{WORKFLOW_URL}/#{@workflow_id}/job")
-      hash = response.parsed
-      return hash.has_key?("workflowID")
-    end
-
-    def wait_for_completion
-      response = @access_token.post("#{WORKFLOW_URL}/events/#{@event_id}")
+    def job_event(job_id)
+      response = @access_token.post("#{@jobs_url}/#{job_id}/event")
       hash = response.parsed
       return hash["status"] == "completed"
     end
